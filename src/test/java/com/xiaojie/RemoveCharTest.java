3ffab88d7933ab6158cbf0078b81732ee81c28db
1874yin <1042712741@qq.com>
2025-04-23 15:12:57 +0800

    add more test demos

--- src/test/java/com/xiaojie/RemoveCharTest.java
+++ src/test/java/com/xiaojie/RemoveCharTest.java
@@ -1,23 +1,30 @@
 package com.xiaojie;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class RemoveCharTest {
 
     String input1 = " aabcccbbad";
     String input2 = "abcccbad";
+    String input3 = "a";
+    String input4 = "";
 
     @Test
     public void testRemove() {
-        Assert.assertEquals("d", RemoveCharacter.remove(input1));
-        Assert.assertEquals("abbad", RemoveCharacter.remove(input2));
+        assertEquals("d", RemoveCharacter.remove(input1));
+        assertEquals("abbad", RemoveCharacter.remove(input2));
+        assertEquals("a", RemoveCharacter.remove(input3));
+        assertEquals("", RemoveCharacter.remove(input4));
     }
 
     @Test
     public void testReplace() {
-        Assert.assertEquals("bad", RemoveCharacter.replace(input1));
-        Assert.assertEquals("d", RemoveCharacter.replace(input2));
+        assertEquals("bad", RemoveCharacter.replace(input1));
+        assertEquals("d", RemoveCharacter.replace(input2));
+        assertEquals("a", RemoveCharacter.replace(input3));
+        assertEquals("", RemoveCharacter.replace(input4));
     }
 }
